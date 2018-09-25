d50d83e479b00da4515dc840b1818d32aca58b5a
Adam <Adam@adhams-mbp.localdomain>
2018-09-25 15:18:22 +0200

    Feedback sounds

--- Music quiz/GameOverViewController.swift
+++ Music quiz/GameOverViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class GameOverViewController: UIViewController {
     
@@ -16,11 +17,24 @@ class GameOverViewController: UIViewController {
     @IBOutlet weak var tryAgainButton: UIButton!
     @IBOutlet weak var backToSelectionButton: UIButton!
     
+    var soundPlayer: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tryAgainButton.layer.cornerRadius = 30
         self.backToSelectionButton.layer.cornerRadius = 30
         self.backToSelectionButton.layer.borderColor = #colorLiteral(red: 0.6, green: 0.6, blue: 0.6, alpha: 1)
         self.backToSelectionButton.layer.borderWidth = 2
+    }
+    
+    func playSuccessSound() {
+        guard let url = Bundle.main.url(forResource: "success", withExtension: "m4a") else { return }
+        soundPlayer = try! AVAudioPlayer(contentsOf: url)
+        soundPlayer?.prepareToPlay()
+        soundPlayer?.play()
+        
+    }
+    @IBAction func tryAgainButton(_ sender: Any) {
+        playSuccessSound()
     }
 }
