5df8080b75e4a3228bdf55477c8d176886335950
Patrik Potoček <patrik.potocek@thefuntasty.com>
2018-09-25 13:44:24 +0200

    Create playlist model

--- Music quiz/Model.swift
+++ Music quiz/Model.swift
@@ -9,6 +9,31 @@
 import Foundation
 import UIKit
 
+class Playlist: NSObject {
+    var name: String!
+    var coverUrl: URL?
+    var tracks = [Track]()
+
+    override init() {
+        //
+    }
+
+    init(json: NSDictionary) {
+        super.init()
+
+        name = json["name"] as! String
+        let images = json["images"] as! [[String: String]]
+        if let image = images.first, let url = image["url"] {
+            coverUrl = URL(string: url)
+        }
+        let array = json.value(forKey: Key.Items.rawValue) as! [NSDictionary]
+        tracks = array.map {
+            let trackObj = $0.value(forKey: "track") as! NSDictionary
+            return Track(json: trackObj)
+        }
+    }
+}
+
 class Artist:NSObject {
     var name :String!
     var id :String!
