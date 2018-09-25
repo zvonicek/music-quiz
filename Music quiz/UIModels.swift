2cca878611ec5770f52f6a3b48bd211150707b93
Patrik Potoček <patrik.potocek@thefuntasty.com>
2018-09-25 11:41:33 +0200

    Create playlist screen and model

---
+++ Music quiz/UIModels.swift
@@ -0,0 +1,14 @@
+//
+//  UIModels.swift
+//  Music quiz
+//
+//  Created by Patrik Potoček on 25.9.18.
+//  Copyright © 2018 The Funtasty. All rights reserved.
+//
+
+import UIKit
+
+struct PlaylistViewModel {
+    let image: UIImage
+    let title, bestScore: String
+}
